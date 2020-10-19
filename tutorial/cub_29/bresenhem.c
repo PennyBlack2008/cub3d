3699b430034ab25c777611b724c92767977bd411
Jinwoo Kang <jikang@c1r5s4.42seoul.kr>
2020-10-19 14:14:55 +0900

    cub_29 new version

--- tutorial/cub_29/bresenhem.c
+++ tutorial/cub_29/bresenhem.c
@@ -1,96 +1,80 @@
-#include "cub_28.h"
+#include "cub_21.h"
 
 /*
 **	bresenhem algorithm
 */
-static void plot_line_low(int p1_x, int p1_y, int p2_x, int p2_y, int color, t_win *w)
+static void plot_line_low(t_plot p1, t_plot p2, int color, t_win *w)
 {
 	int			dx;
 	int			dy;
 	int			yi;
 	int			D;
-	// t_coord		p;
-	int			p_x;
-	int			p_y;
+	t_plot		p;
 
-	p_x = p1_x;
-	p_y = p1_y;
-	dx = p2_x - p1_x;
-	dy = p2_y - p1_y;
+	p.x = p1.x;
+	p.y = p1.y;
+	dx = p2.x - p1.x;
+	dy = p2.y - p1.y;
 	yi = 1;
-	if (dy < 0)
-	{
-		yi = -1;
+	if (dy < 0 && (yi = -1))
 		dy = -dy;
-	}
 	D = 2 * dy - dx;
-	while (p_x <= p2_x)
+	while (p.x <= p2.x)
 	{
-		my_mlx_pixel_put(&w->img, p_x, p_y, color);
-		// mark_pixel(&p_x, &p_y, w);
+		my_mlx_pixel_put(&w->img, p.x, p.y, color);
 		if (D > 0)
 		{
-			p_y += yi;
+			p.y += yi;
 			D -= 2 * dx;
 		}
 		D += 2 * dy;
-		p_x++;
+		p.x++;
 	}
-	mlx_put_image_to_window(w->mlx, w->ptr, w->img.ptr, 0, 0);
 }
 
-static void plot_line_high(int p1_x, int p1_y, int p2_x, int p2_y, int color, t_win *w)
+static void plot_line_high(t_plot p1, t_plot p2, int color, t_win *w)
 {
 	int			dx;
 	int			dy;
 	int			xi;
 	int			D;
-	// t_coord	p;
-	int			p_x;
-	int			p_y;
+	t_plot		p;
 
-	p_x = p1_x;
-	p_y = p1_y;
-	dx = p2_x - p1_x;
-	dy = p2_y - p1_y;
+	p.x = p1.x;
+	p.y = p1.y;
+	dx = p2.x - p1.x;
+	dy = p2.y - p1.y;
 	xi = 1;
-	if (dx < 0)
-	{
-		xi = -1;
+	if (dx < 0 && (xi = -1))
 		dx = -dx;
-	}
 	D = 2 * dx - dy;
-	while (p_y <= p2_y)
+	while (p.y <= p2.y)
 	{
-		my_mlx_pixel_put(&w->img, p_x, p_y, color);
-		// mark_pixel(&p_x, &p_y, w);
+		my_mlx_pixel_put(&w->img, p.x, p.y, color);
 		if (D > 0)
 		{
-			p_x += xi;
-			// D += 2 * (dx - dy);
+			p.x += xi;
 			D -= 2 * dy;
 		}
 		D += 2 * dx;
-		p_y++;
+		p.y++;
 	}
-	mlx_put_image_to_window(w->mlx, w->ptr, w->img.ptr, 0, 0);
 }
 
-void draw_line(int p1_x, int p1_y, int p2_x, int p2_y, int color, t_win *w)
+void draw_line(t_plot p1, t_plot p2, int color, t_win *w)
 {
-	if (abs(p2_x - p1_x) > abs(p2_y - p1_y)) // 기울기 45도 이하일 경우
+	if (fabs(p2.x - p1.x) > fabs(p2.y - p1.y)) // 기울기 45도 이하일 경우
 	{
-		if (p2_x > p1_x)
-			plot_line_low(p1_x, p1_y, p2_x, p2_y, color, w);
+		if (p2.x > p1.x)
+			plot_line_low(p1, p2, color, w);
 		else
-			plot_line_low(p2_x, p2_y, p1_x, p1_y, color, w);
+			plot_line_low(p2, p1, color, w);
 	}
 	else // 기울기 45도 이상일 경우
 	{
-		if (p2_y > p1_y)
-			plot_line_high(p1_x, p1_y, p2_x, p2_y, color, w);
+		if (p2.y > p1.y)
+			plot_line_high(p1, p2, color, w);
 		else
-			plot_line_high(p2_x, p2_y, p1_x, p1_y, color, w);
+			plot_line_high(p2, p1, color, w);
 	}
-	// mlx_put_image_to_window(w->mlx, w->ptr, w->img.ptr, 0, 0);
 }
--- tutorial/cub_29/map.c
+++ tutorial/cub_29/map.c
@@ -1,7 +1,8 @@
-#include "cub_28.h"
-
-#define ROWS 10
+#include "cub_21.h"
+#define ROWS 11
 #define COLS 10
+#define WALL 49 // ascii #1
+#define NOT_WALL 48 // ascii #0
 
 void	map_init(t_win *w)
 {
@@ -17,13 +18,14 @@ void	map_init(t_win *w)
 	w->map.map[0] = "1111111111";
 	w->map.map[1] = "1000001011";
 	w->map.map[2] = "1000100011";
-	w->map.map[3] = "1010111011";
-	w->map.map[4] = "1010001011";
-	w->map.map[5] = "1010001011";
-	w->map.map[6] = "1010001011";
-	w->map.map[7] = "1011100001";
+	w->map.map[3] = "1010000011";
+	w->map.map[4] = "1010000011";
+	w->map.map[5] = "1010000011";
+	w->map.map[6] = "1010000011";
+	w->map.map[7] = "1010000001";
 	w->map.map[8] = "1010000001";
-	w->map.map[9] = "1111111111";
+	w->map.map[9] = "1111000111";
+	w->map.map[10] = "1111111111";
 
 
 }
@@ -34,17 +36,17 @@ void	draw_rectangle(t_win *w, int x, int y, int color)
 	int i, j;
 
 	// x, y 는 각각 원점에서 대각선 위치에 있는 점의 x, y 좌표가 된다. x는 사각형의 너비, y는 사각형의 높이
-	x *= w->R_height / ROWS; // ROWS: 4
-	y *= w->R_width / COLS; // COLS: 10
+	x *= w->wall.length; // ROWS: 4
+	y *= w->wall.length; // COLS: 10
 	// printf("x :%d, y: %d \n", x, y);
 	// 초기값 x
 	i = 0;
-	while (i < w->R_width / COLS)
+	while (i < w->wall.length)
 	{
 		j = 0;
-		while (j < w->R_height / ROWS)
+		while (j < w->wall.length)
 		{
-			my_mlx_pixel_put(&w->img, y + i, x + j, color);
+			my_mlx_pixel_put(&w->img, (y + i) / 4 + w->mini.plot.x, (x + j) / 4 + w->mini.plot.y, color);
 			j++;
 		}
 		i++;
@@ -62,9 +64,6 @@ void	draw_map(t_win *w)
 		j = 0;
 		while (j < COLS)
 		{
-			// printf("%c\n", w->map.map[i][j]);
-			// printf("j: %d\n", j);
-			// printf("i: %d j: %d\n", i, j);
 			if (w->map.map[i][j] == '1')
 				draw_rectangle(w, i, j, 0xFFFFFF);
 			else if (w->map.map[i][j] == '0')
@@ -73,62 +72,19 @@ void	draw_map(t_win *w)
 		}
 		i++;
 	}
-	mlx_put_image_to_window(w->mlx, w->ptr, w->img.ptr, 0, 0);
 }
 
 /*
 ** int						is_wall(int x, int y, t_win *w)
 ** 확인하고자 하는 픽셀의 좌표를 넣으면 그 픽셀이 벽인지 확인 해주는 함수, 1이면 벽, 0이면 벽이 아니다.
-** 픽셀 좌표 x, y 에 각각 TILE_WIDTH, TILE_HEIGHT 를 나누어 이것이 char **map에 어떤 인덱스에
-** 속하는 지 알아보고 map[x * TILE_WIDTH][y * TILE_HEIGHT]가 0이면 벽이아니고 1이면 벽으로 결정
+** 픽셀 좌표 x, y 에 각각 w->wall.length, w->wall.length 를 나누어 이것이 char **map에 어떤 인덱스에
+** 속하는 지 알아보고 map[x * w->wall.length][y * w->wall.length]가 0이면 벽이아니고 1이면 벽으로 결정
 */
 
 int			is_wall(double x, double y, t_win *w)
 {
-	if (w->map.map[(int)(y / TILE_LENGTH)][(int)(x / TILE_LENGTH)] == WALL)
+	if (w->map.map[(int)(y / w->wall.length)][(int)(x / w->wall.length)] == WALL)
 		return (WALL);
-	// printf("w->map.map[%d][%d] = %d\n", (int)(y / TILE_WIDTH), (int)(x / TILE_HEIGHT), w->map.map[(int)(y / TILE_HEIGHT)][(int)(x / TILE_WIDTH)]);
+	// printf("w->map.map[%d][%d] = %d\n", (int)(y / w->wall.length), (int)(x / w->wall.length), w->map.map[(int)(y / w->wall.length)][(int)(x / w->wall.length)]);
 	return (NOT_WALL);
-}
-
-int			is_wall_ray(double x, double y, t_ray *r, t_win *w)
-{
-		if (0 == r->ang)
-		{
-			return(is_wall(x + 1, y, w));
-		}
-		else if (0 < r->ang && r->ang < M_PI_2)
-		{
-			printf("제 1사분면\n");
-			return(is_wall(x + 1, y + 1, w));
-		}
-		else if (M_PI_2 == r->ang)
-		{
-			return(is_wall(x , y + 1, w));
-		}
-		else if (M_PI_2 < r->ang && r->ang < M_PI)
-		{
-			printf("제 2사분면\n");
-			return(is_wall(x - 1, y + 1, w));
-		}
-		else if (M_PI == r->ang)
-		{
-			return(is_wall(x - 1, y, w));
-		}
-		else if (M_PI < r->ang && r->ang < M_PI_2 * 3)
-		{
-			printf("제 3사분면\n");
-			return(is_wall(x - 1, y - 1, w));
-		}
-		else if (M_PI_2 * 3 == r->ang)
-		{
-			return(is_wall(x, y - 1, w));
-		}
-		else if (M_PI_2 * 3 < r->ang && r->ang < 2 * M_PI)
-		{
-			printf("제 4사분면\n");
-			return(is_wall(x + 1, y - 1, w));
-		}
-		
-	return (0);
 }
